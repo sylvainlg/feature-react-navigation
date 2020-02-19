d9ec237ee4674eda56ce74bfd652d77b9623dd06
Sylvain LG <syl@sylvainlg.fr>
2020-02-19 21:17:18 +0100

    build(dev-deps): remove buggy transform-object-rest-spread babel plugin

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,7 @@ module.exports = {
   // standard presets applying various plugins
   presets: ['@babel/preset-env', '@babel/preset-react'],
 
-  plugins: [
-    '@babel/plugin-proposal-class-properties',
-    'transform-object-rest-spread',
-  ],
+  plugins: ['@babel/plugin-proposal-class-properties'],
 
   env: {
     // BABEL_ENV=commonjs ... our CommonJS distribution (promoted in lib/ directory)
